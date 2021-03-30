c38147344d19e42011d0b32d54e40b595eafc8af
Parth Baraiya <parth.b@simformsolutions.com>
2021-03-30 14:55:37 +0530

    Added orientation lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'home.dart';
 
@@ -10,6 +11,7 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown]);
     return MaterialApp(
       home: Home(),
     );
